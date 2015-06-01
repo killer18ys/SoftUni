656821cd7d17b02aad2d9193f82a938b9ee0a9c7
killer18ys <killer18ys@gmail.com>
2015-06-02 00:13:55 +0300

    Adding

---
+++ Console-Input-Output-Homework/P07-SumOf5Numbers/SumOf5Numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P07_SumOf5Numbers
+{
+    class SumOf5Numbers
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string[] numbers =  input.Split(' ');
+            double sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += double.Parse(numbers[i]);
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Console-Input-Output-Homework/P08-NumbersFrom1ToN/NumbersFrom1ToN.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P08_NumbersFrom1ToN
+{
+    class NumbersFrom1ToN
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
