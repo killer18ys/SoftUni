0c749c0bbc8a294db36791f59f942c122550ef07
killer18ys <killer18ys@gmail.com>
2015-06-06 01:40:12 +0300

    adding

---
+++ Console-Input-Output-Homework/P13-WorkHours/WorkHours.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_WorkHours
+{
+    class WorkHours
+    {
+        static void Main(string[] args)
+        {
+            int hoursToFinish = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            percent = percent / 100;
+
+            double daysToWork = days * 12 * 0.9;
+
+            double diff = Math.Floor((daysToWork * percent) - hoursToFinish);
+
+
+            if (diff < 0)
+            {
+                Console.WriteLine("No");
+                Console.WriteLine(diff);
+            }
+            else
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine(diff);
+            }
+
+            
+
+        }
+    }
+}
