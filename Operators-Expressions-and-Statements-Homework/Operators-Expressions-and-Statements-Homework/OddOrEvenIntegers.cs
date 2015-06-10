c203b87d87729ceef47ef777ff8212e14a465887
killer18ys <killer18ys@gmail.com>
2015-06-10 17:10:22 +0300

    Adding Homework Five

---
+++ Operators-Expressions-and-Statements-Homework/Operators-Expressions-and-Statements-Homework/OddOrEvenIntegers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_OddOrEvenIntegers
+{
+    class OddOrEvenIntegers
+    {
+        static void Main(string[] args)
+        {
+            int n  = int.Parse(Console.ReadLine());
+
+            if (n % 2 != 0){
+                Console.WriteLine("{0} is Odd", n);
+            }
+            else{
+                Console.WriteLine("{0} is Even", n);
+            }
+
+        }
+    }
+}
