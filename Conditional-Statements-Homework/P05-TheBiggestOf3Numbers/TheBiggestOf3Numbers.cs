4309af15537289e549a50d8d8ec8b5f1ba579479
killer18ys <killer18ys@gmail.com>
2015-06-19 21:23:21 +0300

    Done with the homework

---
+++ Conditional-Statements-Homework/P05-TheBiggestOf3Numbers/TheBiggestOf3Numbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05_TheBiggestOf3Numbers
+{
+    class TheBiggestOf3Numbers
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    Console.WriteLine("The biggest number is {0}", a);
+                }
+                else
+                {
+                    Console.WriteLine("The biggest number is {0}", c);
+                }
+            }
+            else
+            {
+                if (b > c)
+                {
+                    Console.WriteLine("The biggest number is {0}", b);
+                }
+                else
+                {
+                    Console.WriteLine("The biggest number is {0}", c);
+                }
+
+            }
+
+        }
+    }
+}
