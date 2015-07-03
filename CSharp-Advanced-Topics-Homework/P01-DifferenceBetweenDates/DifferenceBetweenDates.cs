5394ce19bda4f77d95fb462d21a3e6242bba0b0a
killer18ys <killer18ys@gmail.com>
2015-07-03 14:09:47 +0300

    Adding CSharp Advanced

---
+++ CSharp-Advanced-Topics-Homework/P01-DifferenceBetweenDates/DifferenceBetweenDates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_DifferenceBetweenDates
+{
+    class DifferenceBetweenDates
+    {
+        static void Main(string[] args)
+        {
+            string inputOne = Console.ReadLine();
+            string inputTwo = Console.ReadLine();
+
+            DateTime dateOne = DateTime.ParseExact(inputOne, "d.MM.yyyy", CultureInfo.CurrentCulture);
+            DateTime dateTwo = DateTime.ParseExact(inputTwo, "d.MM.yyyy", CultureInfo.CurrentCulture);
+
+
+            System.TimeSpan dateDiff = dateTwo.Subtract(dateOne);
+
+            Console.WriteLine(dateDiff.TotalDays);
+        }
+    }
+}
