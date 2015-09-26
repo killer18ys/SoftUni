898221c05d4124ff9e10fce1c34b873925468339
killer18ys <killer18ys@gmail.com>
2015-09-26 12:21:17 +0300

    Lab

---
+++ Lab-21-09-2015/P01-CensorEmail/CensorEmail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_CensorEmail
+{
+    class CensorEmail
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine(CensorEmailParser(input));
+           
+        }
+
+
+        static string CensorEmailParser(string email)
+        {
+           int usernameLength =  email.IndexOf('@');
+           string placeHolder = new string('*', usernameLength);
+           string username = string.Join("", email.Take(usernameLength).ToArray());
+
+           return email.Replace(username, placeHolder);
+        }
+
+    }
+}
