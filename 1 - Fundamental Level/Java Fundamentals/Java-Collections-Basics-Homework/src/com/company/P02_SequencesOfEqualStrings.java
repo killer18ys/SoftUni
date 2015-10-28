5116185d13bf8f678f58f215b67ad7b44d1daa62
killer18ys <killer18ys@gmail.com>
2015-10-28 13:15:41 +0200

    Adding

--- 1 - Fundamental Level/Java Fundamentals/Java-Collections-Basics-Homework/src/com/company/P02_SequencesOfEqualStrings.java
+++ 1 - Fundamental Level/Java Fundamentals/Java-Collections-Basics-Homework/src/com/company/P02_SequencesOfEqualStrings.java
@@ -12,18 +12,14 @@ public class P02_SequencesOfEqualStrings {
         String[] words = scanner.nextLine().split("\\s+");
 
 
+        System.out.print(words[0]);
         for (int i = 1; i < words.length; i++) {
-            String prevWord = words[i - 1];
-            String currWord = words[i];
-
-            if (prevWord.equals(currWord)){
-
-                System.out.println(prevWord);
-
+            if (words[i].equals(words[i - 1])){
+                System.out.print(" " + words[i]);
             }else{
-                System.out.println(currWord);
+                System.out.println();
+                System.out.print(words[i]);
             }
-
         }
     }
 }
