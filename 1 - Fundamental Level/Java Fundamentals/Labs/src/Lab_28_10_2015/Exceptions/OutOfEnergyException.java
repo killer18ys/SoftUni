921bced4622a69b9b0eb84f2e52c46ce10bc4845
killer18ys <killer18ys@gmail.com>
2015-11-01 22:32:59 +0200

    Finished with the game

---
+++ 1 - Fundamental Level/Java Fundamentals/Labs/src/Lab_28_10_2015/Exceptions/OutOfEnergyException.java
@@ -0,0 +1,10 @@
+package Lab_28_10_2015.Exceptions;
+
+/**
+ * Created by Killer18ys on 28-10-2015.
+ */
+public class OutOfEnergyException extends Exception {
+    public OutOfEnergyException(String message){
+        super(message);
+    }
+}
--- 1 - Fundamental Level/Java Fundamentals/Labs/src/Lab_28_10_2015/Mage.java
+++ 1 - Fundamental Level/Java Fundamentals/Labs/src/Lab_28_10_2015/Mage.java
@@ -1,6 +1,6 @@
 package Lab_28_10_2015;
 
-import Lab_28_10_2015.Exeptions.OutOfEnrgyExeption;
+import Lab_28_10_2015.Exceptions.OutOfEnergyException;
 
 /**
  * Created by Killer18ys on 28-10-2015.
@@ -10,6 +10,8 @@ public class Mage extends GameCharacter {
     private static final int BASE_POWER = 150;
     private static final int BASE_ENERGY = 2500;
     private static final int BASE_ATTACK_COST = 250;
+    private static final double CRITICAL_HIT_THRESHOLD = 0.5;
+    private static final double CHANCE_TO_TELEPORT = 0.5;
 
 
     public Mage(int health, int power, int energy, int attackCost, String name){
@@ -21,7 +23,37 @@ public class Mage extends GameCharacter {
     }
 
     @Override
-    public void attack(GameCharacter other) throws OutOfEnrgyExeption {
+    public void attack(GameCharacter other) throws OutOfEnergyException {
+        if (other == this){
+            throw new IllegalStateException("Cannot attack itself");
+        }
+
+        if(this.getEnergy() < this.getAttackCost()){
+            throw new OutOfEnergyException(this.getName() + " is out of energy. Battle over.");
+        }
+
+        boolean isCritical = Math.random() < CRITICAL_HIT_THRESHOLD;
+        int damage = this.getPower();
+
+        if (isCritical){
+            System.out.println("Critical hit by the Mage.");
+            damage = damage * 2;
+        }
+
+        this.setEnergy(this.getEnergy() - this.getAttackCost());
+
+        other.respond(damage);
+    }
+    @Override
+    public  void respond(int damage){
+        boolean hasTeleported = Math.random() < CHANCE_TO_TELEPORT;
+
+        if (hasTeleported){
+            System.out.println("The mage teleported and evaded the attack.");
+        }else{
+            this.setHealth(this.getHealth() - damage);
+            System.out.println("Successful attack. " + damage + " damage dealt!");
+        }
 
     }
 }
