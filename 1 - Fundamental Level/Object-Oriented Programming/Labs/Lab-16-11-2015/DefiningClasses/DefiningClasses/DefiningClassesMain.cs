4e42f93fc1815b6931ffb830bcc5007072207c89
killer18ys <killer18ys@gmail.com>
2015-11-18 20:13:03 +0200

    adding lads

---
+++ 1 - Fundamental Level/Object-Oriented Programming/Labs/Lab-16-11-2015/DefiningClasses/DefiningClasses/DefiningClassesMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    class DefiningClassesMain
+    {
+        static void Main(string[] args)
+        {
+            Dog unnamed = new Dog();
+            Dog sharo = new Dog("Sharo", "Melez");
+
+
+            unnamed.Breed = "German Shepherd";
+
+            unnamed.Bark();
+            sharo.Bark();
+
+        }
+    }
+}
