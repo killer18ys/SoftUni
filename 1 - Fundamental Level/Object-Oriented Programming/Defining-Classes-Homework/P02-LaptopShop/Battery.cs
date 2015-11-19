6cd870d007f7827730bda2a86b86ced6055c45e1
killer18ys <killer18ys@gmail.com>
2015-11-19 17:41:05 +0200

    problem two solved

---
+++ 1 - Fundamental Level/Object-Oriented Programming/Defining-Classes-Homework/P02-LaptopShop/Battery.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_LaptopShop
+{
+    class Battery
+    {
+        private string batteryType;
+        private double batteryLife;
+
+
+        public Battery(string batteryType, double batteryLife)
+        {
+            this.BatteryType = batteryType;
+            this.BatteryLife = batteryLife;
+        }
+
+        public Battery(string batteryType) 
+            :this(batteryType, 0)
+        {
+            
+        }
+
+        public string BatteryType
+        {
+            get { return this.batteryType;}
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+	            {
+		            throw new ArgumentException("Battery type is null or empty!");
+	            }
+                this.batteryType = value;
+            }
+        }
+
+        public double BatteryLife
+        {
+            get { return this.batteryLife;}
+            set 
+            {
+                if (value < 0)
+	            {
+		            throw new ArgumentOutOfRangeException("Battery life cannot be negative nnumber!");
+	            }
+                this.batteryLife = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            if (this.batteryType != null)
+            {
+                result.Append("Battery type: ").AppendLine(this.BatteryType);   
+            }
+            
+            if (this.BatteryLife > 0)
+            {
+                result.Append("Battery life: ").Append(this.BatteryLife).AppendLine(" hours");
+            }
+
+ 	        return result.ToString();
+        }
+    }
+}
---
+++ 1 - Fundamental Level/Object-Oriented Programming/Defining-Classes-Homework/P02-LaptopShop/Laptop.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02_LaptopShop
+{
+    class Laptop
+    {
+        // model, manufacturer, processor, RAM, graphics card, HDD, screen, battery, battery life in hours and price.
+
+        private string model;
+        private string manufacturer;
+        private string processor;
+        private string ram;
+        private string graphicsCard;
+        private string hdd;
+        private string screen;
+        private Battery battery;   
+        private decimal price;
+
+
+        public Laptop(string model, decimal price)
+        {
+            this.Model = model;
+            this.Price = price;
+        }
+
+        public Laptop(string model, decimal price, string manufacturer = null, string processor = null, string ram = null,
+            string hdd = null, string graphicsCard = null, Battery battery = null, string screen = null )
+            :this(model, price)
+        {
+            this.Manufacturer = manufacturer;
+            this.Processor = processor;
+            this.Ram = ram;
+            this.GraphicsCard = graphicsCard;
+            this.Hdd = hdd;
+            this.Screen = screen;
+            this.Battery = battery;
+        }
+
+
+        public string Model
+        {
+            get { return this.model; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("The model field cannot be null or empty string!");
+                }
+                this.model = value;
+            }
+        }
+
+        public string Manufacturer
+        {
+            get { return this.manufacturer; }
+            set
+            {
+                if (value != null && value.Length <= 0){
+                    throw new ArgumentException("The manufacturer field cannot be empty string");
+                }
+                this.manufacturer = value;
+            }
+        }
+
+        public string Processor
+        {
+            get { return this.processor; }
+
+            set
+            {
+                if (value != null && value.Length <= 0)
+                {
+                    throw new ArgumentException("The prossesor field cannot be empty string!");
+                }
+                this.processor = value;
+            }
+        }
+
+        public string Ram
+        {
+            get { return this.ram; }
+            set
+            {
+                if(value != null && value.Length <= 0){
+                    throw new ArgumentException("The ram field cannot be empty string!");
+                }
+                this.ram = value;
+            }
+
+        }
+
+        public string GraphicsCard
+        {
+            get { return this.graphicsCard; }
+            set
+            {
+                if (value != null && value.Length <= 0)
+                {
+                    throw new ArgumentException("The grafic card field cannot be empty string!");
+                }
+                this.graphicsCard = value;
+
+            }
+        }
+
+        public string Screen
+        {
+            get { return this.screen; }
+            set
+            {
+                if (value != null && value.Length <= 0)
+                {
+                    throw new ArgumentException("The screen field cannot be empty string!");
+                }
+                this.screen = value;
+
+            }
+        }
+
+        public string  Hdd 
+        {
+            get { return this.hdd; }
+            set
+            {
+                if (value != null && value.Length <= 0)
+                {
+                    throw new ArgumentException("The hdd filed cannot be empty string!");
+                }
+                this.hdd = value;
+            }
+        }
+
+        public Battery Battery 
+        { 
+            get {return this.battery;}
+            set { this.battery = value; }
+        }
+
+        public decimal Price
+        {
+            get { return this.price; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("The price cannot be negative number!");
+                }
+                this.price = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.Append("Model: ").AppendLine(this.Model);
+
+            result.Append("Price: ").Append(this.Price).AppendLine(" lv");
+
+            if (this.Manufacturer != null)
+            {
+                result.Append("Manufacturer: ").AppendLine(this.Manufacturer);
+            }
+
+            if (this.Processor != null) 
+            {
+                result.Append("Processor: ").AppendLine(this.Processor);                
+            }
+
+            if (this.Ram != null)
+            {
+                result.Append("Ram: ").AppendLine(this.Ram);
+            }
+
+            if (this.Hdd != null)
+            {
+                result.Append("Hdd: ").AppendLine(this.Hdd);
+            }
+
+            if (this.Battery != null)
+            {
+                result.Append(this.Battery.ToString());
+            }
+
+            if (this.Screen != null)
+            {
+                result.Append("Screen: ").AppendLine(this.Screen);
+            }
+
+            return result.ToString();
+        }
+
+    }
+} 
