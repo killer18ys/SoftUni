55aea0430a111d51e8a93d146692356b081f5b49
killer18ys <killer18ys@gmail.com>
2015-11-29 21:18:13 +0200

    Adding

---
+++ 1 - Fundamental Level/Object-Oriented Programming/InheritanceAndAbstraction/P01_HumanStudentWorker/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_HumanStudentWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student("Tosho1", "Pashev1", "121215023"));
+            students.Add(new Student("Tosho2", "Pashev2", "721215023"));
+            students.Add(new Student("Tosho3", "Pashev3", "321215023"));
+            students.Add(new Student("Tosho4", "Pashev4", "521215023"));
+            students.Add(new Student("Tosho5", "Pashev5", "121215123"));
+            students.Add(new Student("Tosho6", "Pashev6", "421215023"));
+            students.Add(new Student("Tosho7", "Pashev7", "121215023"));
+            students.Add(new Student("Tosho8", "Pashev8", "921215023"));
+            students.Add(new Student("Tosho9", "Pashev9", "621215023"));
+            students.Add(new Student("Tosho10", "Pashev10", "021215023"));
+
+            List<Worker> workers = new List<Worker>();
+
+            workers.Add(new Worker("Stamat1", "Trendafilov1", 300, 12));
+            workers.Add(new Worker("Stamat2", "Trendafilov2", 400, 12));
+            workers.Add(new Worker("Stamat3", "Trendafilov3", 500, 12));
+            workers.Add(new Worker("Stamat4", "Trendafilov4", 600, 12));
+            workers.Add(new Worker("Stamat5", "Trendafilov5", 777, 3));
+            workers.Add(new Worker("Stamat6", "Trendafilov6", 7777, 7));
+            workers.Add(new Worker("Stamat7", "Trendafilov7", 3777, 8));
+            workers.Add(new Worker("Stamat8", "Trendafilov8", 3000, 12));
+            workers.Add(new Worker("Stamat9", "Trendafilov9", 2000, 12));
+            workers.Add(new Worker("Stamat10", "Trendafilov10", 1500, 12));
+
+
+            foreach (Student student in students.OrderBy(s => s.FacultyNumber))
+            {
+                Console.WriteLine(student);
+            }
+
+            Console.WriteLine();
+
+            foreach (Worker worker in workers.OrderByDescending(w => w.MoneyPerHour()))
+            {
+                Console.WriteLine(worker);   
+            }
+
+        }
+    }
+}
---
+++ 1 - Fundamental Level/Object-Oriented Programming/InheritanceAndAbstraction/P03-CompanyHierarchy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P03_CompanyHierarchy.Models;
+
+namespace P03_CompanyHierarchy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Emploee> listOfEmploees = new List<Emploee>();
+
+            listOfEmploees.Add(new Manager(1, "Stamat1", "Toshev1", 733, Department.Sales));
+            listOfEmploees.Add(new Manager(2, "Stamat2", "Toshev2", 333, Department.Production));
+            listOfEmploees.Add(new Manager(3, "Stamat3", "Toshev3", 337, Department.Marketing));
+
+            listOfEmploees.Add(new SalesEmployee(1, "Stamat1", "Toshev1", 733, Department.Sales));
+            listOfEmploees.Add(new SalesEmployee(2, "Stamat2", "Toshev2", 333, Department.Production));
+            listOfEmploees.Add(new SalesEmployee(3, "Stamat3", "Toshev3", 337, Department.Marketing));
+
+            listOfEmploees.Add(new Developer(1, "Stamat1", "Toshev1", 733, Department.Sales));
+            listOfEmploees.Add(new Developer(2, "Stamat2", "Toshev2", 333, Department.Production));
+            listOfEmploees.Add(new Developer(3, "Stamat3", "Toshev3", 337, Department.Marketing));
+
+
+            foreach (var emploee in listOfEmploees)
+            {
+                Console.WriteLine(emploee);
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
