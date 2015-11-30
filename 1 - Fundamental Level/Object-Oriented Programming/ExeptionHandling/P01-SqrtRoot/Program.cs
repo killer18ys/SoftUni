f22fbcf6ee30e888acfb5cfbf01ab0b12f41ec6f
killer18ys <killer18ys@gmail.com>
2015-11-30 02:23:26 +0200

    add

---
+++ 1 - Fundamental Level/Object-Oriented Programming/ExeptionHandling/P01-SqrtRoot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_SqrtRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Math.Sqrt(n);
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+
+        }
+    }
+}
